576f3bd8b9e52becaf3ac2408aefe685e656a6a7
pengyuancong <smartcong@outlook.com>
2017-01-29 13:12:10 +0800

    Initial commit

---
+++ src/main/java/com/izanpin/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.izanpin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by pengyuancong on 2017/1/29.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
