966bca578e29d1126a68af614a9811b4fe1eedf5
SmartCong <smartcong@outlook.com>
2017-02-05 14:52:09 +0800

    Add swagger...and so on...

---
+++ src/main/java/com/izanpin/common/config/MySwaggerConfig.java
@@ -0,0 +1,52 @@
+package com.izanpin.common.config;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * Created by pengyuancong on 2017/2/5.
+ */
+@WebAppConfiguration
+@EnableSwagger
+public class MySwaggerConfig {
+    private SpringSwaggerConfig springSwaggerConfig;
+
+    /**
+     * Required to autowire SpringSwaggerConfig
+     */
+    @Autowired
+    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig)
+    {
+        this.springSwaggerConfig = springSwaggerConfig;
+    }
+
+    /**
+     * Every SwaggerSpringMvcPlugin bean is picked up by the swagger-mvc
+     * framework - allowing for multiple swagger groups i.e. same code base
+     * multiple swagger resource listings.
+     */
+    @Bean
+    public SwaggerSpringMvcPlugin customImplementation()
+    {
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(apiInfo()).includePatterns(
+                ".*?");
+    }
+
+    private ApiInfo apiInfo()
+    {
+        ApiInfo apiInfo = new ApiInfo(
+                "无聊啊",
+                "无聊啊 RESTful API",
+                "izanpin.com",
+                "smartcong@outlook.com",
+                "My Apps API Licence Type",
+                "My Apps API License URL");
+        return apiInfo;
+    }
+}
--- src/main/java/com/izanpin/controller/api/ArticleApiController.java
+++ src/main/java/com/izanpin/controller/api/ArticleApiController.java
@@ -3,6 +3,7 @@ package com.izanpin.controller.api;
 import com.github.pagehelper.PageInfo;
 import com.izanpin.data.ImportData;
 import com.izanpin.service.ArticleService;
+import com.wordnik.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,13 +19,15 @@ public class ArticleApiController {
     ImportData importData;
 
 
+    @ApiOperation(value = "获取Articles")
     @RequestMapping(value = "/{page}/{size}", method = RequestMethod.GET)
     @ResponseBody
     public PageInfo getArticles(@PathVariable Integer page, @PathVariable Integer size) {
         return articleService.getArticles(page, size);
     }
 
-    @RequestMapping(value = "/import", method = RequestMethod.GET)
+    @ApiOperation(value = "导入数据")
+    @RequestMapping(value = "/import", method = RequestMethod.POST)
     @ResponseBody
     public void importData() throws Exception {
         importData.importData();
